6053b7ef061535351b0214935a55b0423ca21f3c
Manojlk66 <144667288+Manojlk66@users.noreply.github.com>
2023-09-20 10:59:30 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,19 @@
+class palyer:
+  def play(self):
+    print("the player is playing cricket")
+
+class Batsman:
+  def play(self):
+    print("the batsman is batting.")
+   
+class Bowler:
+  def play(self):
+     print("the bowler is bowling.")
+
+batsman=Batsman()
+bowler=Bowler()
+
+batsman. play()
+bowler. play()
+
+
